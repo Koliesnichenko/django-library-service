4c23765c20ac60e8068213f9390b3fa50cfa8595
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-03-24 15:46:46 +0200

    create Book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,36 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+
+
+def book_image_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/books/", filename)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=20,
+        choices=[
+            ("HARD", "Hard Cover"),
+            ("SOFT", "Soft Cover"),
+        ],
+        default="SOFT",
+    )
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(decimal_places=2, max_digits=10)
+    image = models.ImageField(null=True, blank=True, upload_to=book_image_path)
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
+
+    def __str__(self):
+        return f"{self.title} - {self.author}"
