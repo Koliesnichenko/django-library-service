e983fe5510c5373f695a5f81a7f375778d44b9e0
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-03-27 14:34:11 +0200

    add borrowings path to urls

--- django_library_service/urls.py
+++ django_library_service/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/books/", include("books.urls", namespace="books")),
     path("api/user/", include("user.urls", namespace="user")),
+    path("api/borrowings/", include("borrowings.urls", namespace="borrowings")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
