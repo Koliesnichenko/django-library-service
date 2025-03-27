f4ddac2603eca71b01e4e42d383687747388515e
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-03-27 12:57:49 +0200

    add views for borrowings

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingSerializer, BorrowingCreateSerializer, ReturnBorrowingSerializer
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.select_related("user", "book").all()
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        if self.action == "retrieve":
+            return BorrowingSerializer
+        if self.action == "return_borrowing":
+            return ReturnBorrowingSerializer
+        return BorrowingSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = super().get_queryset()
+
+        if not user.is_staff:
+            queryset = queryset.filter(user=user)
+
+        is_active = self.request.query_params.get("is_active")
+        user_id = self.request.query_params.get("user_id")
+
+        if is_active is not None:
+            if is_active.lower() == "true":
+                queryset = queryset.filter(actual_return_date__isnull=True)
+            elif is_active.lower() == "false":
+                queryset = queryset.filter(actual_return_date__isnull=False)
+
+        if user.is_staff and user_id:
+            queryset = queryset.filter(user__id=user_id)
+
+        return queryset
+
+    @action(methods=["POST"], detail=True, url_path="return")
+    def return_borrowing(self, request, pk=None):
+        borrowing = self.get_object()
+        serializer = ReturnBorrowingSerializer(borrowing, data={})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
