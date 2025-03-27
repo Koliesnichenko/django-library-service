9a876e2f8c4c40cd95347ab06ccb8301c28048d1
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-03-27 18:39:47 +0200

    some adjustments on books/views

--- books/views.py
+++ books/views.py
@@ -1,6 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import extend_schema, extend_schema_view
-from rest_framework import viewsets, mixins, permissions, status
+from rest_framework import viewsets, mixins, status
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser, AllowAny
