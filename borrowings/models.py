a0630ca39cc93451af5f68f5fdcad0d3232eace5
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-03-28 19:58:05 +0200

    Add fine logic and inventory update to Borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,7 +1,14 @@
+from datetime import date
+
 from django.conf import settings
-from django.db import models
+from django.db import models, transaction
 
 from books.models import Book
+from payments.models import Payment
+
+from payments.utils import create_stripe_payment_session
+
+FINE_MULT = 2
 
 
 class Borrowing(models.Model):
@@ -18,6 +25,45 @@ class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.book.inventory -= 1
+            self.book.save()
+        super().save(*args, **kwargs)
+
+    def return_book(self):
+        if not self.actual_return_date:
+            self.actual_return_date = date.today()
+            self.book.inventory += 1
+            self.save()
+
+    def is_active(self):
+        return not self.actual_return_date
+
+    @transaction.atomic
+    def return_borrowing_with_fine(self, actual_return_date):
+        self.return_book()
+
+        if self.actual_return_date > self.expected_return_date:
+            days_overdue = (
+                actual_return_date - self.expected_return_date
+            ).days
+            fine_amount = days_overdue * self.book.daily_fee * FINE_MULT
+
+            session_id = create_stripe_payment_session(
+                borrowing=self,
+                request=None
+            )
+
+            Payment.objects.create(
+                user=self.user,
+                borrowing=self,
+                amount=fine_amount,
+                status="Pending",
+                session_url=None,
+                session_id=session_id,
+            )
 
     def __str__(self):
         return f"{self.user} borrowed '{self.book.title}' on {self.borrow_date}"
