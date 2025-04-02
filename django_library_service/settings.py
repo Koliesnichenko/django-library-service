149a7d5eb0802ac75482fb927021a8a5ab244c3f
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-04-02 15:54:46 +0300

    add .env.sample

--- django_library_service/settings.py
+++ django_library_service/settings.py
@@ -25,14 +25,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-q_ky1ibq@*e5gc%if5#(qj@f7mpal*=00+vsqib2&+8zf-aokv"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 # Application definition
 
